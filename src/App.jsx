690a975f24693a543138b747c3dce2c899aa36fe
Sudisha-pv <sudhishapv@gmail.com>
2025-07-25 10:25:11 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <>
+      {!loaded && <p>Loading PDF...</p>}
+      <iframe
+        src="/siyad.pdf"
+        width="100%"
+        height="100%"
+        title="PDF"
+        onLoad={() => setLoaded(true)}
+        style={{ position: "absolute", top: 0, left: 0, bottom: 0, right: 0, border: "none" }}
+      ></iframe>
+    </>
+  );
+}
+
+export default App;
