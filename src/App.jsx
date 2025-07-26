f4ca9f4ea767fe0115a935cb9907b8ad0bdc244f
Sudisha-pv <sudhishapv@gmail.com>
2025-07-26 19:41:49 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <iframe
         src="/Malabar_jillakalude_vibhajanam.pdf"
         width="100%"
-        height="100%"
+        height="100%" 
         title="PDF"
         onLoad={() => setLoaded(true)}
         style={{ position: "absolute", top: 0, left: 0, bottom: 0, right: 0, border: "none" }}
